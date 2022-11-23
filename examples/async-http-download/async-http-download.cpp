a57ec205f1fc020764e989b7cfb16a7b800d2391
jack <jack.wgm@gmail.com>
2022-11-23 21:01:30 +0800

    Update example http download

--- examples/async-http-download/async-http-download.cpp
+++ examples/async-http-download/async-http-download.cpp
@@ -1,8 +1,26 @@
 ﻿#include "animals/animals.hpp"
+#include "utils/url_view.hpp"
 namespace net = boost::asio;
 
-int main()
+int main(int argc, char** argv)
 {
+	if (argc != 2)
+	{
+		std::cerr << "Usage: " << argv[0] << " <url>\n";
+		return EXIT_FAILURE;
+	}
+
+	std::string_view url(argv[1]);
+
+	try {
+		urls::url_view(url);
+	}
+	catch (const std::exception& e)
+	{
+		std::cerr << "Url: " << url << " is invalid url\n";
+		return EXIT_FAILURE;
+	}
+
 	net::io_context ioc;
 
 	net::co_spawn(ioc,
@@ -11,7 +29,6 @@ int main()
 			animals::goat g(ioc.get_executor());
 
 			animals::http_request req{ animals::http::verb::get, "", 11 };
-			req.set(animals::http::field::host, "www.boost.org");
 			req.set(animals::http::field::user_agent, ANIMALS_VERSION_STRING);
 
 			g.download_cb([](auto data, auto size)
@@ -23,7 +40,7 @@ int main()
 
 			boost::system::error_code ec;
 			auto resp = co_await g.async_perform(
-				"https://www.boost.org/LICENSE_1_0.txt", req, uawaitable[ec]);
+				std::string(url), req, uawaitable[ec]);
 			if (ec)
 			{
 				LOG_ERR << "http got: " << ec.message();
